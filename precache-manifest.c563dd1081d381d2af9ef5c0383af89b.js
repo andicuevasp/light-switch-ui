c0c2285eb2817245eb461f6a95d353fe4b52b973
andicuevasp <andicuevasp@gmail.com>
2021-09-11 10:07:12 +0200

    Updates

---
+++ precache-manifest.c563dd1081d381d2af9ef5c0383af89b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7234049424e3161ed2ba",
+    "url": "/light-switch-ui/static/css/main.c6297da9.chunk.css"
+  },
+  {
+    "revision": "7234049424e3161ed2ba",
+    "url": "/light-switch-ui/static/js/main.c445f366.chunk.js"
+  },
+  {
+    "revision": "d8fcb313f4c5e45ff85e",
+    "url": "/light-switch-ui/static/js/runtime~main.6b494afe.js"
+  },
+  {
+    "revision": "a68ee97f505489edb6f3",
+    "url": "/light-switch-ui/static/js/2.f65867d9.chunk.js"
+  },
+  {
+    "revision": "c6549eb4000cb29a7c5fad7a793466ef",
+    "url": "/light-switch-ui/index.html"
+  }
+];
